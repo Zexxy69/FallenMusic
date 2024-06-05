983b390edf3c92d0cdba948120cb43e4f89eb14e
Zexxy69 <154735627+Zexxy69@users.noreply.github.com>
2024-06-05 15:09:55 +0530

    Update config.py
    Signed-off-by: Zexxy69 <154735627+Zexxy69@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c7dded1702b9396bbdb88.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/ebf502a0595ba3723dcd3.mp4")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dexterkagrouphaibsdk")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+q-4mZytz9lgzZjM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dexterthingz")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6977149345").split()))
 
